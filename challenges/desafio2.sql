76fb1b9004bdbd3907d8eeb6624c6952a183b27e
Nícolas <nicolasbrga01@gmail.com>
2023-04-04 19:58:50 -0300

    (Fix) Req 2 - Agora vai

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -2,6 +2,6 @@ SELECT COUNT(c.duracao_segundos) 'cancoes',
 COUNT(DISTINCT a.artista) 'artistas',
 COUNT(DISTINCT l.ano_lancamento) 'albuns'
 FROM 
-songs c
-INNER JOIN albums l ON c.album_id = l.album_id
-INNER JOIN artists a ON a.artista_id = l.artista_id;
+SpotifyClone.songs c
+INNER JOIN SpotifyClone.albums l ON c.album_id = l.album_id
+INNER JOIN SpotifyClone.artists a ON a.artista_id = l.artista_id;
